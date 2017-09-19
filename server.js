f7919d48326b8cfe01b0af6abaff36a913717f19
Zel <zelak312@gmail.com>
2017-09-18 22:23:25 -0400

    github pages

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const opn = require("opn");
-
-app.use(express.static("site"));
-
-app.listen(8080, () => {
-    opn("http://localhost:8080");
-});
